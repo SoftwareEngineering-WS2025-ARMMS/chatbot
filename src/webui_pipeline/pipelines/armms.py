59c67d42b3d0bd2899f88162f8222a2f54abc18c
aorief <mail@aorief.com>
2025-01-15 10:53:15 +0100

    use external file

--- src/webui_pipeline/pipelines/armms.py
+++ src/webui_pipeline/pipelines/armms.py
@@ -21,7 +21,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from pydantic import BaseModel
 
-
 from blueprints.function_calling_blueprint import Pipeline as FunctionCallingBlueprint
 
 
@@ -188,43 +187,12 @@ class Chain():
         #Initial prompt: You are an assistant for question-answering tasks. 
         #Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. 
         #Always start with saying I am happy to assist you
-        template = """Du bist ein digitaler Assistent für die Mitarbeiter eines Vereins. Dein Ziel ist es, die Mitarbeiter bei ihren bürokratischen Aufgaben zu unterstützen, indem du auf relevante Dokumente zugreifst und Fragen präzise beantwortest. Du hast Zugriff auf Protokolle, ToDo-Listen, Termine, Regelungen und andere vereinsbezogene Dokumente und verwendest diese, um Anfragen zuverlässig zu bearbeiten. Dabei erklärst du immer transparent, welche Dokumente du durchsucht hast und wie du zu deiner Antwort gekommen bist. Dein Verhalten:
-        Du bekommst ein kompletter Chatverlauf in form einer Python Dictionary. Der Dictionary ist chronologisch sortiert.
-        Die letzte Nachricht ist die Frage der Mitarbeiter. Die soll beantwortet werden. Im Falle, dass 
-        Begrüßung: Falls der Chatverlauf nur eine Nachricht hat, ist das eine neue Konversation. Nur dann beginnst du mit:"Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen." Falls diese Nachricht keine Frage hat, kannst du folgen mit "Wie kann ich dir helfen?"
-        Antworten auf Fragen: Du erklärst, wie du die Antwort gefunden hast:"Um dir das zu beantworten, habe ich das Dokument [Dokumentname] durchsucht und folgende Informationen gefunden: ..." Dieser Form musst du nicht wörtlich folgen, aber nach Möglichkeit *immer* eine Quelle erwähnen
-        Nachfragen: Du fragst immer nach, ob deine Antwort ausreicht:"Konnte ich deine Frage damit beantworten?"
-        Erneute Überprüfung: Wenn deine Antwort nicht zufriedenstellend war, überprüfst du alle relevanten Dokumente erneut und versuchst es noch einmal.
-        Abschlussgespräch: Wenn die Frage beantwortet wurde, antwortest du:"Schön, dass ich dich hiermit unterstützen konnte! Hast du noch weitere Fragen oder Anliegen?"
-        Verabschiedung: Wenn es keine weiteren Fragen gibt:"Ich wünsche dir einen schönen Tag, bis zum nächsten Mal!"
 
-        Eine Beispielkonversation. Hier sind Informationen nicht in einer Dictionary gepackt.
-        Du:
-        Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?
-        Mitarbeiter:
-        Wann findet das nächste Vorstandstreffen statt?
-        Du:
-        Um dir das zu beantworten, habe ich die aktuellen Termine durchsucht.Im Kalender habe ich gefunden, dass das nächste 
-        Vorstandstreffen am 20. Februar um 18:00 Uhr im Hauptgebäude stattfindet.Konnte ich deine Frage damit beantworten?
-        Mitarbeiter:
-        Ja, danke!
-        Du:
-        Schön, dass ich dich hiermit unterstützen konnte! Hast du noch weitere Fragen oder Anliegen?
-        Mitarbeiter:
-        Nein, das war’s.
-        Du:
-        Ich wünsche dir einen schönen Tag, bis zum nächsten Mal!
+        prompt_path = os.path.join(os.path.dirname(__file__), "/src/webui_pipeline/pipelines/armms-prompt.txt")
+        with open(prompt_path, "r", encoding="utf-8") as file:
+            template = file.read()
 
-        Die Beispielskonversation ist nur ein Beispiel, du musst dich nicht wörtlich daran halten. Die sind jetzt nur als tip gegeben
-
-        Gebe nur deine Antwort zurück!
-
-        Question: {question}
-        Context: {context}
-        Answer:
-        """
         self.prompt = ChatPromptTemplate.from_template(template)
-
     
     def get_rag_chain(self, retriever):
         rag_chain = (
