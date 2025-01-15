51cdd8988c06eaf4af72f629cf1d5b933f64beda
aorief <mail@aorief.com>
2025-01-15 10:15:40 +0100

    fix tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -19,7 +19,8 @@ class TestPipeline(unittest.TestCase):
 
         This will be then later used by the pipe
         """
-        body = asyncio.run(self.pipeline.inlet({}, {'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}], "oauth_sub": "12345"}))
+        body = asyncio.run(self.pipeline.inlet({'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]},
+                                                {"oauth_sub": "12345"}))
 
         self.assertIn("oauth_sub", body["user_additional_info"])
     
@@ -30,22 +31,23 @@ class TestPipeline(unittest.TestCase):
     
     def test_pipe_malformed_sub(self):
 
-        body = asyncio.run(self.pipeline.inlet({}, {"oauth_sub": "12345", 'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]}))
+        body = asyncio.run(self.pipeline.inlet({'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]},
+                                                {"oauth_sub": "12345"}))
         answer = self.pipeline.pipe("Hello", "-", "-", body)
         
         self.assertIn("Error", answer)
     
     def test_pipe_invalid_user(self):
-
-        body = asyncio.run(self.pipeline.inlet({}, {"oauth_sub": "oidc@123", 'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]}))
+        body = asyncio.run(self.pipeline.inlet({'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]},
+                                               {"oauth_sub": "oidc@123"}))
         answer = self.pipeline.pipe("Hello", "-", "-", body)
         
         self.assertIn("Error", answer)
     
 
     def test_pipe_valid_user_connected_to_storage(self):
-
-        body = asyncio.run(self.pipeline.inlet({}, {"oauth_sub": "oidc@8284579b-93a6-49ec-a598-d84440fe5774", 'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]}))
+        body = asyncio.run(self.pipeline.inlet({'messages': [{'role': 'user', 'content': 'Hallo, wie kannst du mir unterstützen'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting?'}, {'role': 'assistant', 'content': 'Hallo, ich freue mich, dich bei deiner heutigen Tätigkeit im Verein zu unterstützen. Wie kann ich dir helfen?'}, {'role': 'user', 'content': 'Wann war das letzte Meeting'}]},
+                                                 {"oauth_sub": "oidc@8284579b-93a6-49ec-a598-d84440fe5774"}))
         answer = self.pipeline.pipe("Hello", "-", "-", body)
         
         self.assertNotIn("Error", answer)
